776a15ec0cba3bc81363bed7e8170312f8470fb6
ddochea0314 <ddochea@outlook.com>
2021-01-19 19:52:10 +0900

    - 간략한 메일 발송 서비스 관련 예제 구현프로젝트

---
+++ ETC/MailSendExample/MailSendExample/Program.cs
@@ -0,0 +1,54 @@
+﻿using DnsClient;
+using MimeKit;
+using System;
+using System.Linq;
+
+namespace MailSendExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lookup = new LookupClient();
+            var sendDomain = "senddomain.com";
+            var sender = $"noreply@{sendDomain}";
+            var recDomain = "gmail.com"; // <<input your mail domain>>
+            var recName = "<<input your mail account>>"; // <<input your mail account>>
+            var result = lookup.Query(recDomain, QueryType.MX);
+
+            var record = result.Answers.MxRecords().FirstOrDefault();
+            var domain = record?.Exchange.Value;
+
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Joey Tribbiani", sender));
+            message.To.Add(new MailboxAddress(recName, $"{recName}@{recDomain}"));
+            message.Subject = "How you doin'?";
+
+            message.Body = new TextPart("plain")
+            {
+                Text = @"Hey Chandler,
+
+I just wanted to let you know that Monica and I were going to go play some paintball, you in?
+
+-- Joey"
+            };
+            using (var client = new MailKit.Net.Smtp.SmtpClient())
+            {
+                client.LocalDomain = sendDomain;
+                client.ServerCertificateValidationCallback = (s, c, h, e) => {
+                    return true;
+                };
+                try
+                {
+                    client.Connect(domain, 0, MailKit.Security.SecureSocketOptions.Auto);
+                }
+                catch (Exception ex)
+                {
+
+                }
+                client.Send(message);
+                client.Disconnect(true);
+            }
+        }
+    }
+}
