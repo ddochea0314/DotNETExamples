7c19b7dc2eb5c74e6f1903f87b05eca29b2b3628
ddochea0314 <ddochea@outlook.com>
2021-01-21 21:07:27 +0900

    axios로 Array 형식의 데이터파라메터 전달시 FormQuery 처리 방법예시 소스 (Name 설정)

---
+++ Web/Get/Get.FormQuery/Controllers/ValuesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Get.FormQuery.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<int> Get([FromQuery(Name = "numbers[]")] IEnumerable<int> numbers)
+        {
+            return numbers.Select(v => v + 100);
+        }
+    }
+}
---
+++ Web/Get/Get.FormQuery/clientapp/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <button @click="getdata">click me</button>
+    <div :key="result" v-for="result in results">
+      {{result}}
+    </div>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+  },
+  data: () => ({
+    results: [],
+    numbers: [1,2,3,4,5]
+  }),
+  methods: {
+    getdata() {
+      axios.get("/api/values", {
+        params: {
+          numbers: this.numbers
+        }
+      }).then(res => {
+        this.results = res.data;
+      });
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
