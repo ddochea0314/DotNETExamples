54cd2eb715e7baaaa28f4b71dc30cfb89df06c3c
ddochea <ddochea0314@gmail.com>
2021-02-24 09:00:31 +0900

    Docker 개발환경 구축 예제

---
+++ Docker/DockerEnvTest1/DockerEnvTest1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DockerEnvTest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine($"OS : ${Environment.OSVersion}");
+        }
+    }
+}
