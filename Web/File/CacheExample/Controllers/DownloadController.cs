c32effb68e540072eb0cd49b598d7587a42ce17d
ddochea0314 <ddochea@outlook.com>
2021-04-12 23:22:11 +0900

    - 캐시 응답 적용 예제 추가

---
+++ Web/File/CacheExample/Controllers/DownloadController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CacheExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DownloadController : ControllerBase
+    {
+        // GET 방식으로 다운로드를 받는다. 파일명에 특수문자가 있다면 URLEncoding을 통해 파일명을 인코딩하여 호출해야한다. 
+        // 구축환경에 IIS인 경우, URLEncoding 했음에도, + 기호와 공란이 제대로 구분 못할 수 있다. 해결방법은 사이트 참조. (https://ddochea.tistory.com/75)
+        [HttpGet("{filename}")]
+        [ResponseCache(Duration = 15)]
+        public async Task<IActionResult> GetDownloadResult(string filename)
+        {
+            var path = Path.Join("Download", filename);
+            if (System.IO.File.Exists(path))
+            {
+                byte[] bytes;
+                using (FileStream file = new FileStream(path: path, mode: FileMode.Open)) // 배포환경에선 다운로드폴더에 대한 권한설정작업이 필요할 수 있다.
+                {
+                    try
+                    {
+                        bytes = new byte[file.Length];
+                        await file.ReadAsync(bytes);
+                        return File(bytes, "application/octet-stream");
+                    }
+                    catch (Exception ex)
+                    {
+                        return StatusCode(StatusCodes.Status500InternalServerError);
+                    }
+                }
+
+
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ Web/File/CacheExample/Controllers/WeatherForecastController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CacheExample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [ResponseCache(Duration = 15)]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        //[HttpGet]
+        //public IEnumerable<WeatherForecast> Get()
+        //{
+        //    var rng = new Random();
+        //    Response.Headers.Append("Cache-control", "max-age=15"); // 원시적이지만 모든 종류의 웹 서버(Spring framework, expressJS, Django 등)에서 공통적으로 사용가능한 방식
+        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateTime.Now.AddDays(index),
+        //        TemperatureC = rng.Next(-20, 55),
+        //        Summary = Summaries[rng.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+        //}
+
+        //[HttpPost]
+        //[ResponseCache(Duration = 15)] // Post는 이정도 설정으론 캐시가 동작하지 않는다.
+        //public IEnumerable<WeatherForecast> Post()
+        //{
+        //    var rng = new Random();
+        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateTime.Now.AddDays(index),
+        //        TemperatureC = rng.Next(-20, 55),
+        //        Summary = Summaries[rng.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+        //}
+
+
+        [HttpPost]
+        [ResponseCache(Duration = 15)] // 여전히 동작안함. safe 메소드에 한해 적용가능하다곤 하나 방법을 모름.
+        public IActionResult Post()
+        {
+            // https://tools.ietf.org/html/rfc7231#section-4.2.1
+            var rng = new Random(); 
+            var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+            Response.Headers.Append("Expires", DateTime.Now.AddDays(1).ToString("R"));
+            Response.Headers.Append("x-readonly", "true");
+            return Ok(result);
+        }
+    }
+
+}
