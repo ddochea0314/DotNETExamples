54b16db52f130cae32cc76f45b6bfe7869ff063c
ddochea0314 <ddochea@outlook.com>
2021-04-17 16:40:34 +0900

    범위 연산자 수정

--- ETC/BetterCodes/BetterCodes/Program.cs
+++ ETC/BetterCodes/BetterCodes/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 
 namespace BetterCodes
 {
@@ -6,8 +9,12 @@ namespace BetterCodes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Substring이 간소화 되었네요.".Substring(0, 9));
-            Console.WriteLine("Substring이 간소화 되었네요."[0..9]);
+            Console.WriteLine("Substring_기존 방식".Substring(0, 9));
+            Console.WriteLine("Substring_C# 8부터 사용가능한 방식"[0..9]); // 범위연산자. C# 8.0부터 추가
+            Console.WriteLine("Substring_C# 8부터 사용가능한 방식"[..9]); // 0..9와 같음
+
+            // https://docs.microsoft.com/ko-kr/dotnet/csharp/whats-new/csharp-8#indices-and-ranges
+
         }
     }
 }
