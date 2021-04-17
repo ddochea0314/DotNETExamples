2c7734b56a6739f435cf21349e624bc5dde1fafd
ddochea0314 <ddochea@outlook.com>
2021-04-17 16:39:52 +0900

    범위 연산자 예제 추가

---
+++ ETC/BetterCodes/BetterCodes/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BetterCodes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Substring이 간소화 되었네요.".Substring(0, 9));
+            Console.WriteLine("Substring이 간소화 되었네요."[0..9]);
+        }
+    }
+}
